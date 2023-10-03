17cd87ed930e648057db57d851ec01f7287dee12
Karisma98 <karismaswain80@gmail.com>
2023-10-03 17:38:41 +0530

    karisma

--- src/Containers/Leave/Child/CardView/StatusLeavesForm.js
+++ src/Containers/Leave/Child/CardView/StatusLeavesForm.js
@@ -1,15 +1,9 @@
-import React,{useEffect} from 'react';
+
+import React from 'react';
 import { ClockCircleOutlined } from '@ant-design/icons';
 import { Timeline } from 'antd';
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import {getApprovedStatusByTaskId } from "../../LeavesAction"
 
-const StatusLeavesForm = (props) => {
-  useEffect(() => {
-    props.getApprovedStatusByTaskId(props.taskId);
-   
-  }, []);
+const StatusLeavesForm = () => (
   <Timeline
     items={[
       {
@@ -28,19 +22,61 @@ const StatusLeavesForm = (props) => {
       },
     ]}
   />
-  };
+);
 
-const mapStateToProps = ({ leave, auth }) => ({
-  userId: auth.userDetails.userId,
-  approveStatus:leave.approveStatus,
-})
+export default StatusLeavesForm;
 
-const mapDispatchToProps = (dispatch) =>
-bindActionCreators(
-  {
-    getApprovedStatusByTaskId
-  },
-  dispatch,
-)
 
-export default connect(mapStateToProps, mapDispatchToProps)(StatusLeavesForm)
+
+
+
+
+
+
+
+// import React,{useEffect} from 'react';
+// import { ClockCircleOutlined } from '@ant-design/icons';
+// import { Timeline } from 'antd';
+// import { connect } from 'react-redux'
+// import { bindActionCreators } from 'redux'
+// // import {getApprovedStatusByTaskId } from "../../LeavesAction"
+
+// const StatusLeavesForm = (props) => {
+//   // useEffect(() => {
+//   //   props.getApprovedStatusByTaskId(props.taskId);
+   
+//   // }, []);
+//   <Timeline
+//     items={[
+//       {
+//         children: 'Create a services site 2015-09-01',
+//       },
+//       {
+//         children: 'Solve initial network problems 2015-09-01',
+//       },
+//       {
+//         dot: <ClockCircleOutlined className="timeline-clock-icon" style={{color:"red"}} />,
+//         color: 'red',
+//         children: 'Technical testing 2015-09-01',
+//       },
+//       {
+//         children: 'Network problems being solved 2015-09-01',
+//       },
+//     ]}
+//   />
+//   };
+
+// const mapStateToProps = ({ leave, auth }) => ({
+//   // userId: auth.userDetails.userId,
+//   // approveStatus:leave.approveStatus,
+// })
+
+// const mapDispatchToProps = (dispatch) =>
+// bindActionCreators(
+//   {
+//     // getApprovedStatusByTaskId
+//   },
+//   dispatch,
+// )
+
+// export default connect(mapStateToProps, mapDispatchToProps)(StatusLeavesForm)
