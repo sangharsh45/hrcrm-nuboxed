f7612adc544febc8538c4dcc0ad72fae4b5f73ad
karisma <you@example.com>
2023-10-31 16:45:46 +0530

    karisma

--- src/Containers/Pitch/PitchAction.js
+++ src/Containers/Pitch/PitchAction.js
@@ -490,7 +490,7 @@ export const getPitch = (userId) => (dispatch) => {
   export const addPitchNote = (note, cb) => (dispatch) => {
     dispatch({ type: types.ADD_PITCH_NOTES_REQUEST });
     axios
-      .post(`${base_url}/pitch/notes`, note, {
+      .post(`${base_url}/investorleads/notes`, note, {
         headers: {
           Authorization: "Bearer " + sessionStorage.getItem("token") || "",
         },
@@ -513,12 +513,12 @@ export const getPitch = (userId) => (dispatch) => {
       });
   };
 
-  export const getNotesListByPitchId = (leadsId) => (dispatch) => {
+  export const getNotesListByPitchId = (investorLeadsId) => (dispatch) => {
     dispatch({
       type: types.GET_NOTES_LIST_BY_PITCH_ID_REQUEST,
     });
     axios
-      .get(`${base_url}/leads/note/${leadsId}`, {
+      .get(`${base_url}/investorleads/note/${investorLeadsId}`, {
         headers: {
           Authorization: "Bearer " + sessionStorage.getItem("token") || "",
         },
