6fe98ec5e4e56087ca055bd39f7d7d6ecc4d6907
silpa82603 <82430661+silpa82603@users.noreply.github.com>
2023-12-27 17:23:15 +0530

    27-12-23 new changes

--- src/Containers/Main/Refurbish/RefurbishActionRight.js
+++ src/Containers/Main/Refurbish/RefurbishActionRight.js
@@ -8,18 +8,18 @@ const RefurbishActionRight = () => {
     const exportTemplateCMRPage = async () => {
         var doc = new jsPDF();
 
-        var name1 = `HEADER 1 :`
-        var name2 = `HEADER 2 :`
+        var name1 = `HEADER 1`
+        var name2 = `HEADER 2`
         var value = "4"
         var header = `Exemplar für
 Copy for`
 
         doc.setFillColor(2, 179, 34);
-        doc.rect(0, 0, 230, 16, 'F');
+        doc.rect(0, 3, 230, 16, 'F');
         doc.setFontSize(25);
-        doc.text(value, 3, 10)
+        doc.text(value, 6, 10)
         doc.setFontSize(12);
-        doc.text(header, 11, 6)
+        doc.text(header, 13, 6)
 
         doc.setFontSize(10);
         doc.setDrawColor(0, 255, 0)
@@ -39,109 +39,172 @@ Copy for`
     const exportPDFCMRPage = async () => {
         var doc = new jsPDF();
 
-        var name1 = `HEADER 1 :`
-        var name2 = `HEADER 2 :`
-        var name3 = `HEADER 3 :`
-        var name4 = `HEADER 4 :`
-        var name5 = `HEADER 5 :`
-        var name6 = `HEADER 6 :`
-        var name7A = `HEADER 7-A :`
-        var name7B = `HEADER 7-B :`
-        var name8 = `HEADER 8 :`
-        var name9A = `HEADER 9A`
-        var name9B = `HEADER 9B`
-        var name9C = `HEADER 9C`
-        var name9D = `HEADER 9D`
-        var name10A = `HEADER 10A`
-        var name10B = `HEADER 10B`
-        var name10C = `HEADER 10C`
-        var name11A = `HEADER 11A`
-        var name11B = `HEADER 11B`
-        var name11C = `HEADER 11C`
+        var name1 = `1`
+        var name2 = `No CMR`
+        var name3 = `2`
+        var name4 = `16`
+        var name5 = `3`
+        var name6 = `17`
+        var name7A = `4`
+        var name7B = `5`
+        var name8 = `18`
+        var name9A = `6`
+        var name9B = `7`
+        var name9C = `8`
+        var name9D = `9`
+        var name10A = `10`
+        var name10B = `11`
+        var name10C = `12`
+        var name11A = `HA`
+        var name11B = `HB`
+        var name11C = `HC`
+        var name13 = `13`
+        var name19 = `19`
+        var name14 = `14`
+        var name21 = `21`
+        var name15 = `15`
+        var name20 = `20`
+        var name22 = `22`
+        var name23 = `23`
+        var name24 = `24`
         var value = "4"
         var header = `Exemplar für
 Copy for`
 
         doc.setFillColor(2, 179, 34);
-        doc.rect(0, 0, 230, 16, 'F');
+        doc.rect(0, 0, 230, 13, 'F');
         doc.setFontSize(25);
-        doc.text(value, 3, 10)
+        doc.text(value, 3, 8)
         doc.setFontSize(12);
-        doc.text(header, 11, 6)
+        doc.text(header, 11, 5)
 
         //box-1
         doc.setFontSize(10);
         doc.setDrawColor(0, 255, 0)
-        doc.rect(5, 16, 100, 30); // x, y, width, height //make box 2
-        doc.text(name1, 30, 20);
-        doc.text("Content/Text of box -1", 8, 26);
+        doc.rect(5, 13, 100, 22); // x, y, width, height //make box 2
+        doc.text(name1, 8, 19);
+        // doc.text("Content/Text of box -1", 8, 23);
         //box-2
         doc.setDrawColor(0, 255, 0)
-        doc.rect(105, 16, 100, 30); ///make box 1
-        doc.text(name2, 140, 20);
-        doc.text("Content/Text of box -2", 110, 26);
-
+        doc.rect(105, 13, 100, 22); ///make box 1
+        doc.text(name2, 110, 19);
         //box-3
         doc.setFontSize(10);
         doc.setDrawColor(0, 255, 0)
-        doc.rect(5, 46, 100, 30); // x, y, width, height //make box 2
-        doc.text(name3, 30, 50);
-        doc.text("Content/Text of box -3", 8, 56);
+        doc.rect(5, 35, 100, 22); // x, y, width, height //make box 2
+        doc.text(name3, 8, 40);
+        // doc.text("Content/Text of box -3", 8, 43);
         // box 4
         doc.setDrawColor(0, 255, 0)
-        doc.rect(105, 46, 100, 30); ///make box 1
-        doc.text(name4, 140, 50);
-        doc.text("Content/Text of box -4", 110, 56);
+        doc.rect(105, 35, 100, 22); ///make box 1
+        doc.text(name4, 110, 40);
+        // doc.text("Content/Text of box -4", 110, 43);
         //box -5
         doc.setFontSize(10);
         doc.setDrawColor(0, 255, 0)
-        doc.rect(5, 76, 100, 30); // x, y, width, height //make box 2
-        doc.text(name5, 30, 80);
-        doc.text("Content/Text of box -5", 8, 86);
+        doc.rect(5, 57, 100, 22); // x, y, width, height //make box 2
+        doc.text(name5, 8, 60);
+
         //box -6
         doc.setDrawColor(0, 255, 0)
-        doc.rect(105, 76, 100, 30); ///make box 1
-        doc.text(name6, 140, 80);
-        doc.text("Content/Text of box -6", 110, 86);
+        doc.rect(105, 57, 100, 22); ///make box 1
+        doc.text(name6, 110, 60);
+
         // box -7A
         doc.setFontSize(10);
         doc.setDrawColor(0, 255, 0)
-        doc.rect(5, 106, 100, 20); // x, y, width, height //make box 2
-        doc.text(name7A, 30, 112);
-        doc.text("Content/Text of box -7-A", 8, 117);
+        doc.rect(5, 79, 100, 13); // x, y, width, height //make box 2
+        doc.text(name7A, 8, 82);
+
         // //box-7B
         doc.setFontSize(10);
         doc.setDrawColor(0, 255, 0)
-        doc.rect(5, 106, 100, 38); // x, y, width, height //make box 2
-        doc.text(name7B, 30, 130);
-        doc.text("Content/Text of box -7-B", 8, 135);
+        doc.rect(5, 92, 100, 13); // x, y, width, height //make box 2
+        doc.text(name7B, 8, 97);
+
         //box -8
         doc.setDrawColor(0, 255, 0)
-        doc.rect(105, 106, 100, 38);
-        doc.text(name8, 140, 110);
-        doc.text("Content/Text of box -8", 110, 120);
+        doc.rect(105, 79, 100, 26);
+        doc.text(name8, 110, 82);
+
         //box-9
         doc.setFontSize(10);
         doc.setDrawColor(0, 255, 0)
-        doc.rect(5, 144, 115, 90); // x, y, width, height //make box 2
-        doc.text(name9A, 7, 150);
-        doc.text(name9B, 36, 150);
-        doc.text(name9C, 63, 150);
-        doc.text(name9D, 90, 150);
+        doc.rect(5, 105, 115, 50); // x, y, width, height //make box 2
+        doc.text(name9A, 7, 113);
+        doc.text(name9B, 36, 113);
+        doc.text(name9C, 63, 113);
+        doc.text(name9D, 90, 113);
         // box-10
-        doc.rect(120, 144, 28, 105);
-        doc.rect(148, 144, 28, 105);
-        doc.rect(176, 144, 29, 105);
-        doc.text(name10A, 123, 150);
-        doc.text(name10B, 152, 150);
-        doc.text(name10C, 180, 150);
+        doc.rect(120, 105, 28, 62);
+        doc.rect(148, 105, 28, 62);
+        doc.rect(176, 105, 29, 62);
+        doc.text(name10A, 123, 113);
+        doc.text(name10B, 152, 113);
+        doc.text(name10C, 180, 113);
         // box-11
         doc.setDrawColor(0, 255, 0)
-        doc.rect(5, 234, 115, 15);
-        doc.text(name11A, 20, 238);
-        doc.text(name11B, 50, 238);
-        doc.text(name11C, 80, 238);
-        // doc.text("box-9-A", 8, 135);
+        doc.rect(5, 155, 115, 12);
+        doc.text(name11A, 20, 163);
+        doc.text(name11B, 50, 163);
+        doc.text(name11C, 80, 163);
+        // //box -12
+
+        doc.rect(5, 167, 100, 53);
+        doc.text(name13, 20, 175);
+
+        //box 13 table
+        doc.text(name19, 108, 171);
+        doc.rect(105, 167, 25, 53);
+        doc.rect(130, 167, 25, 53);
+        doc.rect(155, 167, 25, 53);//column
+        doc.rect(180, 167, 25, 53);
+
+        doc.rect(105, 167, 100, 7);
+        doc.rect(105, 174, 100, 14); //row
+        doc.rect(105, 188, 100, 26);
+        doc.rect(105, 214, 100, 6);
+
+        doc.rect(130, 174, 75, 7); //sub-row-row2
+        doc.rect(130, 181, 75, 7);
+
+        doc.rect(130, 188, 75, 6); //sub-row-row3
+        doc.rect(130, 194, 75, 7);
+        doc.rect(130, 201, 75, 6); //sub-row
+        doc.rect(130, 207, 75, 7);
+
+        //box-14
+
+        doc.rect(5, 220, 200, 9);
+        doc.text(name14, 8, 224);
+
+        // box -15
+        doc.rect(5, 229, 100, 11); // x, y, width, height //make box 2
+        doc.text(name15, 8, 235);
+
+        //box-16
+        doc.rect(5, 240, 100, 11); // x, y, width, height //make box 2
+        doc.text(name21, 8, 244);
+
+        //box -17        
+        doc.rect(105, 229, 100, 22);
+        doc.text(name20, 110, 233);
+
+        // box -18
+        doc.rect(5, 251, 67, 25); // x, y, width, height //make box 2
+        doc.text(name22, 8, 255);
+
+        //box-19
+        doc.rect(72, 251, 66, 25); // x, y, width, height //make box 2
+        doc.text(name23, 77, 255);
+
+        //box -20       
+        doc.rect(138, 251, 67, 25);
+        doc.text(name24, 140, 257);
+
+        //footer
+        doc.setFillColor(2, 179, 34);
+        doc.rect(0, 276, 230, 15, 'F');
 
         doc.save(`CMR ${moment().format("L")}`);
 
