c82fdfa054053e352cac0498773c958342f130cb
Dibya391 <dibyaranjan828@gmail.com>
2024-01-02 19:54:57 +0530

    drb5change

---
+++ src/Containers/Procurement/Child/ProcurementTCForm.js
@@ -0,0 +1,187 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Button } from "antd";
+import { Formik, Form, Field } from "formik";
+import { TextareaComponent } from "../../../Components/Forms/Formik/TextareaComponent";
+
+class ProcurementTCForm extends Component {
+
+    componentDidMount = () => {
+        // this.props.getTermsNConditionInOrder(this.props.projectOrderId)
+    }
+
+    render() {
+
+        return (
+            <>
+                <Formik
+                    enableReinitialize
+                    initialValues={{
+                        // termsOfPayment: orderTermsnCondition.termsOfPayment === "null" ? "" : orderTermsnCondition.termsOfPayment,
+                        // applicant: orderTermsnCondition.applicant === "null" ? "" : orderTermsnCondition.applicant,
+                        // offerValidity: orderTermsnCondition.offerValidity === "null" ? "" : orderTermsnCondition.offerValidity,
+                        // deliveryTerms: orderTermsnCondition.deliveryTerms === "null" ? "" : orderTermsnCondition.deliveryTerms,
+                        // installationTerms: orderTermsnCondition.installationTerms === "null" ? "" : orderTermsnCondition.installationTerms,
+                        // beneficiary: orderTermsnCondition.beneficiary === "null" ? "" : orderTermsnCondition.beneficiary,
+                    }}
+                    //   validationSchema={CallSchema}
+                    onSubmit={(values, { resetForm }) => {
+
+                        this.props.addTnCInOrder(
+                            {
+                                ...values,
+                                // projectOrderId: this.props.projectOrderId,
+                                // userId: this.props.userId
+                            },
+                            // this.props.projectOrderId
+                        );
+                    }}
+                >
+                    {({
+                        errors,
+                        touched,
+                        isSubmitting,
+                        setFieldValue,
+                        setFieldTouched,
+                        values,
+                        ...rest
+                    }) => (
+                        <Form>
+                            <div style={{ display: "flex", justifyContent: "space-between", margin: "20px 0 5px 5px" }}>
+                                <div style={{ width: "30%" }}>
+                                    <Field
+                                        name="termsOfPayment"
+                                        label="Terms of Payment"
+                                        isColumn
+                                        width={"100%"}
+                                        component={TextareaComponent}
+                                        inlineLabel
+                                        style={{
+                                            flexBasis: "80%",
+                                            height: "80px",
+                                            marginTop: "0px",
+                                        }}
+                                    />
+                                </div>
+
+                                <div style={{ width: "30%" }}>
+                                    <Field
+                                        name="applicant"
+                                        label="Applicant/Buyer"
+                                        isColumn
+                                        width={"100%"}
+                                        component={TextareaComponent}
+                                        inlineLabel
+                                        style={{
+                                            flexBasis: "80%",
+                                            height: "80px",
+                                            marginTop: "0px",
+                                        }}
+                                    />
+                                </div>
+                                <div style={{ width: "30%" }}>
+                                    <Field
+                                        name="offerValidity"
+                                        label="Offer Validity"
+                                        isColumn
+                                        width={"100%"}
+                                        component={TextareaComponent}
+                                        inlineLabel
+                                        style={{
+                                            flexBasis: "80%",
+                                            height: "80px",
+                                            marginTop: "0px",
+                                        }}
+                                    />
+                                </div>
+
+                            </div>
+                            <div style={{ display: "flex", justifyContent: "space-between", margin: "10px 0 5px 5px" }}>
+                                <div style={{ width: "30%" }}>
+                                    <Field
+                                        name="deliveryTerms"
+                                        label="Delivery Terms"
+                                        isColumn
+                                        width={"100%"}
+                                        component={TextareaComponent}
+                                        inlineLabel
+                                        style={{
+                                            flexBasis: "80%",
+                                            height: "80px",
+                                            marginTop: "0px",
+                                        }}
+                                    />
+                                </div>
+
+                                <div style={{ width: "30%" }}>
+                                    <Field
+                                        name="installationTerms"
+                                        label="Installation Terms:"
+                                        isColumn
+                                        width={"100%"}
+                                        component={TextareaComponent}
+                                        inlineLabel
+                                        style={{
+                                            flexBasis: "80%",
+                                            height: "80px",
+                                            marginTop: "0px",
+                                        }}
+                                    />
+                                </div>
+                                <div style={{ width: "30%" }}>
+                                    <Field
+                                        name="beneficiary"
+                                        label="Beneficiary / Consigner / Manufacturer"
+                                        isColumn
+                                        width={"100%"}
+                                        component={TextareaComponent}
+                                        inlineLabel
+                                        style={{
+                                            flexBasis: "80%",
+                                            // height: "80px",
+                                            marginTop: "0px",
+                                        }}
+                                    />
+                                </div>
+
+                            </div>
+                        
+                            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                                <Button
+                                    type="primary"
+                                    htmlType="submit"
+                                    // loading={this.props.addingTnCInOrder}
+                                >
+                                    Update
+                                </Button>
+                            </div>
+                      
+
+
+                        </Form>
+                    )}
+                </Formik>
+
+            </>
+        );
+    }
+}
+
+const mapStateToProps = ({ order, auth }) => ({
+    // orderTermsnCondition: order.orderTermsnCondition,
+    // userId: auth.userDetails.userId,
+    // addingTnCInOrder: order.addingTnCInOrder
+});
+
+const mapDispatchToProps = (dispatch) =>
+    bindActionCreators(
+        {
+            // addTnCInOrder,
+            // getTermsNConditionInOrder
+        },
+        dispatch
+    );
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProcurementTCForm);
+
--- src/Containers/Procurement/Child/ProcurementTab.js
+++ src/Containers/Procurement/Child/ProcurementTab.js
@@ -5,6 +5,10 @@ import { TabsWrapper } from "../../../Components/UI/Layout";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 const ProcurementCard=lazy(()=>import("./ProcurementCard"));
+const ProcurementTCForm=lazy(()=>import("./ProcurementTCForm"));
+const ProcurementIndentCard=lazy(()=>import("./ProcurementIndentCard"));
+const ProcurementQuotationCard=lazy(()=>import("./ProcurementQuotationCard"));
+const ProcurementPurchaseCard=lazy(()=>import("./ProcurementPurchaseCard"));
 
 const TabPane = StyledTabs.TabPane;
 
@@ -48,8 +52,127 @@ class ProcurementTab extends Component {
                 <ProcurementCard />
               </Suspense>
             </TabPane>
+            <TabPane
+              tab={
+                <>
+                  <span>
+                    
+                  T&C
+                  </span>
+                
+                </>
+              }
+              key="2"
+            >
+              <Suspense fallback={"Loading ..."}>
+                {" "}
+                <ProcurementTCForm/>
+              </Suspense>
+            </TabPane>
+            <TabPane
+              tab={
+                <>
+                  <span>
+                    
+                  Indent
+                  </span>
+                
+                </>
+              }
+              key="3"
+            >
+              <Suspense fallback={"Loading ..."}>
+                {" "}
+                <ProcurementIndentCard/>
+              </Suspense>
+            </TabPane>
+            <TabPane
+              tab={
+                <>
+                  <span>
+                    
+                  Inquiry
+                  </span>
+                
+                </>
+              }
+              key="4"
+            >
+              <Suspense fallback={"Loading ..."}>
+                {" "}
+                <ProcurementQuotationCard/>
+              </Suspense>
+            </TabPane>
 
-           
+              <TabPane
+              tab={
+                <>
+                  <span>
+                    
+                 Purchase Order
+                  </span>
+                
+                </>
+              }
+              key="5"
+            >
+              <Suspense fallback={"Loading ..."}>
+                {" "}
+               <ProcurementPurchaseCard/>
+              </Suspense>
+            </TabPane>
+         
+                 <TabPane
+              tab={
+                <>
+                  <span>
+                    
+                 CI
+                  </span>
+                
+                </>
+              }
+              key="6"
+            >
+              <Suspense fallback={"Loading ..."}>
+                {" "}
+               CIk
+              </Suspense>
+            </TabPane>
+                 <TabPane
+              tab={
+                <>
+                  <span>
+                    
+               Dispatch
+                  </span>
+                
+                </>
+              }
+              key="7"
+            >
+              <Suspense fallback={"Loading ..."}>
+                {" "}
+               Dispatch
+              </Suspense>
+            </TabPane>
+                 <TabPane
+              tab={
+                <>
+                  <span>
+                    
+               Stock
+                  </span>
+                
+                </>
+              }
+              key="8"
+            >
+              <Suspense fallback={"Loading ..."}>
+                {" "}
+               Stock
+              </Suspense>
+            </TabPane>
           </StyledTabs>
         </TabsWrapper>
       
