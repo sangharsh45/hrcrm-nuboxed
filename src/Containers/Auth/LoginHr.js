a4c0e33134c99e343b82d0f6d38eb41f3417e5c2
saumya-lapi <srjjack9@gmail.com>
2024-02-21 17:40:11 +0530

    saumya//done

--- src/Containers/Auth/LoginHr.js
+++ src/Containers/Auth/LoginHr.js
@@ -93,18 +93,18 @@ class LoginHr extends Component {
               margin: "auto",
             }}
           >
-            <img
+            {/* <img
               className="big-logo"
               src={FWLogo}
               style={{ width: 200 }}
               alt="Tekorero logo"
-            />
-            {/* <img
+            /> */}
+            <img
               className="big-logo"
               src={FWLogo2}
               style={{ width: 200 }}
               alt="Tekorero logo"
-            /> */}
+            />
             <br />
      
             <FormWrapper width="25em">
