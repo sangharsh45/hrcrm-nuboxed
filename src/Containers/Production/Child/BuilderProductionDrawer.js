7871998faf024a98431d88e45d41c120fbd63677
silpa82603 <82430661+silpa82603@users.noreply.github.com>
2024-03-06 13:32:27 +0530

    06-03-2024 new changes

--- src/Containers/Production/Child/BuilderProductionDrawer.js
+++ src/Containers/Production/Child/BuilderProductionDrawer.js
@@ -1,5 +1,5 @@
 
-import React, { lazy, Component,Suspense } from "react";
+import React, { lazy, Component, Suspense } from "react";
 import { StyledDrawer } from "../../../Components/UI/Antd";
 import { FormattedMessage } from "react-intl";
 import { BundleLoader } from "../../../Components/Placeholder";
@@ -7,28 +7,24 @@ const ProductionBldrCrd = lazy(() => import("./ProductionBldrCrd"));
 
 class BuilderProductionDrawer extends Component {
   render() {
-    const { openbUILDERProductiondrawer, handleBuilderProduction,particularDiscountData, ...formProps } = this.props;
+    const { openbUILDERProductiondrawer, handleBuilderProduction, particularDiscountData, ...formProps } = this.props;
     const isSmallScreen = window.innerWidth <= 600;
     const drawerWidth = isSmallScreen ? "90%" : "60%";
     return (
       <>
         <StyledDrawer
-          title={<FormattedMessage
-            id="app.production"
-            defaultMessage="Product Builder"
-          />}
-
+          title={`Product Builder for ${particularDiscountData.manufactureId}`}
           width={drawerWidth}
           visible={openbUILDERProductiondrawer}
           onClose={() => handleBuilderProduction(false)}
           footer={null}
         >
 
-              
-                <Suspense fallback={<BundleLoader/>}>
-                <ProductionBldrCrd particularDiscountData={particularDiscountData}/>
-                </Suspense>
-      
+
+          <Suspense fallback={<BundleLoader />}>
+            <ProductionBldrCrd particularDiscountData={particularDiscountData} />
+          </Suspense>
+
 
         </StyledDrawer>
       </>
